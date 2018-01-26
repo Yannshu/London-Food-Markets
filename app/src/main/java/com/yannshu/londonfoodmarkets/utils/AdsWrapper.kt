980344b7df4c2d2ca955e15e2c377835ae291323
Yann Viens <viensyann@gmail.com>
2018-01-26 21:21:55 +0000

    Move key/ids to gradle & build config.

--- app/src/main/java/com/yannshu/londonfoodmarkets/utils/AdsWrapper.kt
+++ app/src/main/java/com/yannshu/londonfoodmarkets/utils/AdsWrapper.kt
@@ -4,12 +4,12 @@ import android.content.Context
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdView
 import com.google.android.gms.ads.MobileAds
-import com.yannshu.londonfoodmarkets.R
+import com.yannshu.londonfoodmarkets.BuildConfig
 
 class AdsWrapper {
 
     fun init(context: Context) {
-        MobileAds.initialize(context, context.getString(R.string.google_admob_app_id))
+        MobileAds.initialize(context, BuildConfig.google_admob_app_id)
     }
 
     fun loadAd(adView: AdView) {
