94c80b0f505a0dcc844be5ec63f5e5ecc9e79545
Yann Viens <viensyann@gmail.com>
2018-02-16 20:53:48 +0000

    Add a SharedPrefsWrapper.

---
+++ app/src/main/java/com/yannshu/londonfoodmarkets/utils/SharedPreferencesWrapper.kt
@@ -0,0 +1,20 @@
+package com.yannshu.londonfoodmarkets.utils
+
+import android.content.Context
+
+class SharedPreferencesWrapper(context: Context, name: String = APP_SHARED_PREFERENCES) {
+
+    companion object {
+        private const val APP_SHARED_PREFERENCES = ""
+    }
+
+    private val sharedPreferences = context.getSharedPreferences(APP_SHARED_PREFERENCES, Context.MODE_PRIVATE)
+
+    fun saveFloat(key: String, value: Float) = sharedPreferences.edit().putFloat(key, value).apply()
+
+    fun getFloat(key: String, defaultValue: Float = 0.0f) = sharedPreferences.getFloat(key, defaultValue)
+
+    fun saveLong(key: String, value: Long) = sharedPreferences.edit().putLong(key, value).apply()
+
+    fun getLong(key: String, defaultValue: Long = 0L) = sharedPreferences.getLong(key, defaultValue)
+}
