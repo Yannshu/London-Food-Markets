c61146f6b0720ccf6a5cf12f137e4b0cf97cfb97
Yann Viens <viensyann@gmail.com>
2018-08-28 20:00:12 +0100

    Rename events.

--- app/src/main/java/com/yannshu/londonfoodmarkets/utils/analytics/AnalyticsEvent.kt
+++ app/src/main/java/com/yannshu/londonfoodmarkets/utils/analytics/AnalyticsEvent.kt
@@ -1,8 +1,8 @@
 package com.yannshu.londonfoodmarkets.utils.analytics
 
 object AnalyticsEvent {
-    const val MARKET_VIEWED = "market viewed"
-    const val ABOUT_VIEWED = "about viewed"
-    const val SUGGEST_MISSING_MARKET_VIEWED = "suggest missing market viewed"
+    const val MARKET_VIEWED = "market_viewed"
+    const val ABOUT_VIEWED = "about_viewed"
+    const val SUGGEST_MISSING_MARKET_VIEWED = "suggest_missing_market_viewed"
     const val ARG_NAME = "name"
 }
